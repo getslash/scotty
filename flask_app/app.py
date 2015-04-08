379a212e817de600b6e20e4b7b946b782d40a19a
Roey Darwish Dror <roeyd@infinidat.com>
2015-04-08 14:02:16 +0300

    Use postgresql

--- flask_app/app.py
+++ flask_app/app.py
@@ -15,7 +15,7 @@ app = flask.Flask(__name__, static_folder=os.path.join(ROOT_DIR, "..", "static")
 app.config["SECRET_KEY"] = ""
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.path.expandvars(
-    os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:////tmp/__demo_db.sqlite'))
+    os.environ.get('SQLALCHEMY_DATABASE_URI', 'postgresql://localhost/scotty'))
 
 
 _CONF_D_PATH = os.environ.get('CONFIG_DIRECTORY', os.path.join(ROOT_DIR, "..", "conf.d"))
