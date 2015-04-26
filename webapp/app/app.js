1774d6598bc030b31dd9ea3771189d332c504040
Roey Darwish Dror <roeyd@infinidat.com>
2015-04-26 15:34:02 +0300

    INFRADEV-4495: Periodically refresh the beam window

--- webapp/app/app.js
+++ webapp/app/app.js
@@ -11,6 +11,36 @@ var App = Ember.Application.extend({
   Resolver: Resolver
 });
 
+App.Pollster = Ember.Object.extend({
+  interval: function() {
+    return 5000; // Time between polls (in ms)
+  }.property().readOnly(),
+
+  // Schedules the function `f` to be executed every `interval` time.
+  schedule: function(f) {
+    return Ember.run.later(this, function() {
+      if (f.apply(this)) {
+        this.set('timer', this.schedule(f));
+      }
+    }, this.get('interval'));
+  },
+
+  // Stops the pollster
+  stop: function() {
+    Ember.run.cancel(this.get('timer'));
+  },
+
+  // Starts the pollster, i.e. executes the `onPoll` function every interval.
+  start: function() {
+    this.set('timer', this.schedule(this.get('onPoll')));
+  },
+
+  onPoll: function(){
+    // Issue JSON request and add data to the store
+  }
+});
+
 loadInitializers(App, config.modulePrefix);
 config.torii.providers['google-oauth2'].redirectUri = window.location.origin;
 export default App;
+
