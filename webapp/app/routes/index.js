ba8b9b0c6e3539c88e021a91c4be6cc5d106f197
Roey Darwish Dror <roeyd@infinidat.com>
2015-04-28 08:55:00 +0300

    Beams are refreshed automatically

--- webapp/app/routes/index.js
+++ webapp/app/routes/index.js
@@ -1,9 +1,25 @@
 import Ember from 'ember';
 import Materialize from '../mixins/materialize';
+import App from '../app';
 
 export default Ember.Route.extend(Materialize, {
   model: function() {
     return this.store.find('beam');
   },
 
+  afterModel: function() {
+    this._super();
+    var self = this;
+
+    if (Ember.isNone(this.get('pollster'))) {
+      this.set('pollster', App.Pollster.create({
+        onPoll: function() {
+          self.store.fetchAll("beam");
+          return true;
+        }
+      }));
+      this.get('pollster').start();
+      this.get('pollster').set("interval", 1000 * 60 * 5);
+    }
+  }
 });
