9273098b847a312665218a6c0a451a9a3fac09c4
Roey Darwish Dror <roeyd@infinidat.com>
2015-10-28 19:07:34 +0200

    Move to a more clean serializer API

--- webapp/app/serializers/beam.js
+++ webapp/app/serializers/beam.js
@@ -1,11 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
-  keyForAttribute: function(key, method) {
-    if (key === "error_message") {
-      return "error";
-    } else {
-      return this._super(key, method);
-    }
+  attrs: {
+    error_message: "error",
   }
 });
