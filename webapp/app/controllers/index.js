94f6821c0fcc4c6bd5afa168a2d638e79f1a2361
Roey Darwish Dror <roeyd@infinidat.com>
2015-11-03 15:48:45 +0200

    Fix dynamic sorting of the index page

--- webapp/app/controllers/index.js
+++ webapp/app/controllers/index.js
@@ -2,4 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   application: Ember.inject.controller('application'),
+  sortKeys: ['start:desc'],
+  sortedModel: Ember.computed.sort('model', 'sortKeys')
 });
