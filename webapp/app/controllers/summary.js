8fd69dd6c781dc027d8a4129f06b3df1cbdd6c5e
Roey Darwish Dror <roeyd@infinidat.com>
2015-11-04 15:55:23 +0200

    Bump version

--- webapp/app/controllers/summary.js
+++ webapp/app/controllers/summary.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   space_percent: function() {
-    return (this.get("model.used_space") / this.get("model.total_space") * 100).toFixed(0);
+    return Math.round(this.get("model.used_space") / this.get("model.total_space") * 100);
   }.property("model.total_space", "model.used_space")
 });
