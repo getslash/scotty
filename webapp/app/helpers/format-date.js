6d4d79333c5fe906f0c26e1b60283c6bccf6facb
Roey Darwish Dror <roeyd@infinidat.com>
2015-11-25 11:23:02 +0200

    Fix format-date for empty dates

--- webapp/app/helpers/format-date.js
+++ webapp/app/helpers/format-date.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 /* global moment */
 
 export function formatDate(params/*, hash*/) {
+  if (params[0] === null) {
+    return '';
+  }
   return moment(params[0]).format('DD/MM/YY HH:mm');
 }
 
