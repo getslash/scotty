1fbe9ac35ab8a2876aeeeaf7765cc7f3b36aab4f
Roey Darwish Dror <roeyd@infinidat.com>
2016-02-10 10:57:02 +0200

    Forbid beaming up the root directory (fix #16)

--- tests/test_deployment/test__sanity.py
+++ tests/test_deployment/test__sanity.py
@@ -1,8 +1,17 @@
 import subprocess
-
+import http.client
+import slash
+from requests.exceptions import HTTPError
 
 def test_sanity(scotty):
     scotty.sanity_check()
+
+
+def test_forbid_root_beam(scotty):
+    with slash.assert_raises(HTTPError) as e:
+        scotty.beam_up("/")
+
+    assert e.exception.response.status_code == http.client.CONFLICT
 
 
 def test_independent_beam(beam, local_beam_dir, download_dir):
