9101be8ebf6cca8f950536d498425b082b6deda4
Roey Darwish Dror <roeyd@infinidat.com>
2016-03-02 09:00:04 +0200

    Use ember-concurrency
    Replace all the callbacks and awaits with Ember concurrency

--- webapp/app/app.js
+++ webapp/app/app.js
@@ -13,55 +13,6 @@ App = Ember.Application.extend({
   Resolver
 });
 
-App.Pollster = Ember.Object.extend({
-  interval: function() {
-    return 5000; // Time between polls (in ms)
-  }.property(),
-
-  // Schedules the function `f` to be executed every `interval` time.
-  schedule: function(f) {
-    return Ember.run.later(this, function() {
-      if (f.apply(this)) {
-        this.set('timer', this.schedule(f));
-      }
-    }, this.get('interval'));
-  },
-
-  // Stops the pollster
-  stop: function() {
-    Ember.run.cancel(this.get('timer'));
-  },
-
-  // Starts the pollster, i.e. executes the `onPoll` function every interval.
-  start: function() {
-    this.set('timer', this.schedule(this.get('onPoll')));
-  },
-
-  onPoll: function() {
-    // Issue JSON request and add data to the store
-  }
-});
-
-App.instanceInitializer({
-  name: "relative_time_update",
-
-  initialize: function(instance) {
-    let storage = instance.lookup("service:store");
-    App.update_reltime = App.Pollster.create({
-      onPoll: function() {
-        let beams = storage.peekAll("beam").content;
-        for (var i = 0; i < beams.length; i++) {
-          let beam = beams[i].getRecord();
-          beam.set("tick", beam.get("tick") + 1);
-        }
-        return true;
-      }
-    });
-    App.update_reltime.set("interval", 60000);
-    App.update_reltime.start();
-  }
-});
-
 loadInitializers(App, config.modulePrefix);
 config.torii.providers['google-oauth2'].redirectUri = window.location.origin;
 export default App;
