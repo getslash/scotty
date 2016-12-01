bedfc0b90c9b25ed8fdead9d009dba5e5eb16806
Roey Darwish Dror <roeyd@infinidat.com>
2016-12-01 18:57:47 +0200

    Fix naming convention in user model

--- webapp/app/models/user.js
+++ webapp/app/models/user.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   email: DS.attr('string'),
-  display_name: function() {
+  displayName: function() {
     if (this.get('name')) {
       return this.get('name');
     }
