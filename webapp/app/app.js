6c4f3fe18c984bc0246eafb76048f451f1daac88
Roey Darwish Dror <roeyd@infinidat.com>
2018-04-16 11:18:22 +0300

    Upgrade Ember

--- webapp/app/app.js
+++ webapp/app/app.js
@@ -1,13 +1,9 @@
-import Ember from 'ember';
+import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-let App;
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
-App = Ember.Application.extend({
+const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
@@ -15,4 +11,5 @@ App = Ember.Application.extend({
 
 loadInitializers(App, config.modulePrefix);
 config.torii.providers['google-oauth2'].redirectUri = window.location.origin;
+
 export default App;
