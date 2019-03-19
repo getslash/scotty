a9014470ab244f9b1525709472f4ccfc61538d1f
Dor Amram <damram@infinidat.com>
2019-03-19 13:40:43 +0200

    Fix upgrade script

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -5,9 +5,10 @@ set -ex
 sudo apt-get update && apt-get upgrade
 sed -i 's/jessie/stretch/g' /etc/apt/sources.list
 sudo apt-get update && apt-get upgrade && apt-get dist-upgrade
+sudo apt install python3-venv
 sed -i 's/python3.4/python3.5/g' _lib/bootstrapping.py
 sed -i 's/virtualenv/venv/g' _lib/bootstrapping.py
-sudo rm -rf /apt/scotty/src/.env/
+sudo rm -rf /opt/scotty/src/.env/
 systemctl restart scotty-wsgi
-sudo apt install python3-venv
+read
 systemctl restart scotty-celery-worker scotty-celery-beat
