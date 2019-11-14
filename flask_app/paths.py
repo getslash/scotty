1823990a525dc2f66ff6833b113ff825c7c0d20e
Dor Amram <damram@infinidat.com>
2019-11-14 13:30:34 +0200

    INFRADEV-12655: Recognize the host's operating system and download the proper combadge

--- flask_app/paths.py
+++ flask_app/paths.py
@@ -1,10 +1,14 @@
 import os
 
 COMBADGE_PATH_V1 = os.path.join(os.path.dirname(__file__), "..", "webapp", "dist", "assets", "combadge.py")
-COMBADGE_PATH_V2 = os.path.join(os.path.dirname(__file__), "..", "combadge", "target", "x86_64-unknown-linux-musl", "release", "combadge")
+COMBADGE_ASSETS_DIR = os.path.join(os.path.dirname(__file__), "..", "combadge_assets")
+COMBADGE_PATH_LINUX = os.path.join(COMBADGE_ASSETS_DIR "combadge_linux")
+COMBADGE_PATH_WINDOWS = os.path.join(COMBADGE_ASSETS_DIR "combadge_windows")
+COMBADGE_PATH_DARWIN = os.path.join(COMBADGE_ASSETS_DIR "combadge_darwin")
 
-
-_COMBADGE_PATHS = {'v2': COMBADGE_PATH_V2,
+_COMBADGE_PATHS = {'linux': COMBADGE_PATH_LINUX,
+                   'windows': COMBADGE_PATH_WINDOWS,
+                   'darwin': COMBADGE_PATH_DARWIN,
                    'v1': COMBADGE_PATH_V1}
 
 
