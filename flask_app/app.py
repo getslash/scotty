c1f65035ee7859941d8a94ca88db392eb230c1cf
DorAmram <doramram210@gmail.com>
2019-12-04 11:19:07 +0200

    INFRADEV-13192: Update YAML loading method (#91)

--- flask_app/app.py
+++ flask_app/app.py
@@ -50,7 +50,7 @@ def create_app(config=None):
     for yaml_path in configs:
         if os.path.isfile(yaml_path):
             with open(yaml_path) as yaml_file:
-                app.config.update(yaml.load(yaml_file))
+                app.config.update(yaml.load(yaml_file, Loader=yaml.FullLoader))
 
     app.config.update(config)
 
