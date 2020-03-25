334e4ccd7f813fd0dca3be9e7e02ca1e523028e0
David Sternlicht <dsternlicht@infinidat.com>
2020-03-25 10:18:56 +0200

    Fix formatting

--- flask_app/blueprints/auth.py
+++ flask_app/blueprints/auth.py
@@ -53,7 +53,7 @@ def get_or_create_user(email, name):
 @auth.route("/login", methods=["POST"])
 def login():
     flow = flow_from_clientsecrets(
-        filename=current_app.config['CLIENT_SECRET_PATH'],
+        filename=current_app.config["CLIENT_SECRET_PATH"],
         scope="https://www.googleapis.com/auth/userinfo.profile",
         redirect_uri=request.json["redirectUri"],
     )
