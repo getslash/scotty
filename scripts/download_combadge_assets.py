b62d2600e9315970adeb11a55fab2b2f238a3e93
David Sternlicht <d1618033@gmail.com>
2020-10-12 12:51:26 +0300

    Refactor download artifacts (#133)
    Co-authored-by: David Sternlicht <dsternlicht@infinidat.com>

--- scripts/download_combadge_assets.py
+++ scripts/download_combadge_assets.py
@@ -6,7 +6,11 @@ import zipfile
 import requests
 
 
-artifacts = requests.get('https://api.github.com/repos/getslash/scotty/actions/artifacts').json()['artifacts']
+artifacts_response = requests.get('https://api.github.com/repos/getslash/scotty/actions/artifacts')
+artifacts_response.raise_for_status()
+artifacts_json = artifacts_response.json()
+assert 'artifacts' in artifacts_json, f"Artifacts: {artifacts_json}"
+artifacts = artifacts_json['artifacts']
 auth = (os.environ['GITHUB_USERNAME'], os.environ['GITHUB_PASSWORD'])
 artifacts = sorted(artifacts, key=lambda x: datetime.datetime.strptime(x['updated_at'], '%Y-%m-%dT%H:%M:%SZ'), reverse=True)[:3]
 
@@ -14,7 +18,9 @@ combadge_assets = os.path.join(os.path.dirname(__file__), os.pardir, 'combadge_a
 os.makedirs(combadge_assets, exist_ok=True)
 
 for artifact in artifacts:
-    content = requests.get(artifact['archive_download_url'], auth=auth).content
+    artifact_content_response = requests.get(artifact['archive_download_url'], auth=auth)
+    artifact_content_response.raise_for_status()
+    content = artifact_content_response.content
     file = io.BytesIO(content)
     file.seek(0)
     with zipfile.ZipFile(file) as z:
