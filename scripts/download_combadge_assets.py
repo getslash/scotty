fb0a17f63e02dc4939cb77507fde72dad689e1e6
David Sternlicht <d1618033@gmail.com>
2020-10-13 11:06:20 +0300

    Use token in download combadge instead of user/pass (#135)
    Co-authored-by: David Sternlicht <dsternlicht@infinidat.com>

--- scripts/download_combadge_assets.py
+++ scripts/download_combadge_assets.py
@@ -11,14 +11,17 @@ artifacts_response.raise_for_status()
 artifacts_json = artifacts_response.json()
 assert 'artifacts' in artifacts_json, f"Artifacts: {artifacts_json}"
 artifacts = artifacts_json['artifacts']
-auth = (os.environ['GITHUB_USERNAME'], os.environ['GITHUB_PASSWORD'])
+token = os.environ["GITHUB_ACCESS_TOKEN"]
 artifacts = sorted(artifacts, key=lambda x: datetime.datetime.strptime(x['updated_at'], '%Y-%m-%dT%H:%M:%SZ'), reverse=True)[:3]
 
 combadge_assets = os.path.join(os.path.dirname(__file__), os.pardir, 'combadge_assets', 'v2')
 os.makedirs(combadge_assets, exist_ok=True)
 
 for artifact in artifacts:
-    artifact_content_response = requests.get(artifact['archive_download_url'], auth=auth)
+    artifact_content_response = requests.get(
+        artifact['archive_download_url'],
+        headers={"Authorization": f"token {token}"}
+    )
     artifact_content_response.raise_for_status()
     content = artifact_content_response.content
     file = io.BytesIO(content)
