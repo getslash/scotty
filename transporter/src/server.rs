1c31b22c55c297015b5148066826cf3b14f91fd5
David Sternlicht <d1618033@gmail.com>
2021-10-31 10:03:53 +0200

    Fix formatting (#141)
    * Fix formatting
    
    * Fix linting
    
    * Remove anyjson
    
    * Fix linting
    
    Co-authored-by: David Sternlicht <dsternlicht@infinidat.com>

--- transporter/src/server.rs
+++ transporter/src/server.rs
@@ -16,41 +16,39 @@ pub fn listen(storage: FileStorage, bind_address: &str, scotty_url: &str) -> Res
 
     debug!("Debug messages are on");
     info!("Listening for connections in {}", &bind_address);
-    for stream in listener.incoming() {
-        if let Ok(stream) = stream {
-            let storage = storage.clone();
-            let mut tags = vec![];
-            let scotty_url = String::from(scotty_url);
+    for stream in listener.incoming().flatten() {
+        let storage = storage.clone();
+        let mut tags = vec![];
+        let scotty_url = String::from(scotty_url);
 
-            match stream.peer_addr() {
-                Err(why) => error!("Cannot get peer address for socket: {}", why),
-                Ok(peer) => tags.push(("peer".to_string(), format!("{}", peer))),
-            };
-            thread::spawn(move || {
-                if let Err(why) = beam_up(stream, storage, Scotty::new(scotty_url), &mut tags) {
-                    if !why.is_disconnection() {
-                        let event = {
-                            let mut event = Event::new();
-                            event.level = sentry::Level::Error;
-                            event.message = Some(format!("{}", why));
+        match stream.peer_addr() {
+            Err(why) => error!("Cannot get peer address for socket: {}", why),
+            Ok(peer) => tags.push(("peer".to_string(), format!("{}", peer))),
+        };
+        thread::spawn(move || {
+            if let Err(why) = beam_up(stream, storage, Scotty::new(scotty_url), &mut tags) {
+                if !why.is_disconnection() {
+                    let event = {
+                        let mut event = Event::new();
+                        event.level = sentry::Level::Error;
+                        event.message = Some(format!("{}", why));
 
-                            let mut tag_map = Map::new();
-                            for tag in tags {
-                                let (key, value) = tag;
-                                tag_map.insert(key, value);
-                            }
+                        let mut tag_map = Map::new();
+                        for tag in tags {
+                            let (key, value) = tag;
+                            tag_map.insert(key, value);
+                        }
 
-                            event.tags = tag_map;
-                            event
-                        };
+                        event.tags = tag_map;
+                        event
+                    };
 
-                        sentry::capture_event(event);
-                    }
-
-                    error!("Connection closed: {}", why);
+                    sentry::capture_event(event);
                 }
-            });
-        }
+
+                error!("Connection closed: {}", why);
+            }
+        });
     }
 
     drop(listener);
